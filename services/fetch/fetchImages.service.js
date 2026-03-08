9b72c924c1e6f12be3131e62f3672cf2bbf81cca
Kumar Aniket <ka344057@gmail.com>
2026-03-08 16:57:56 +0530

    first working version of my_cloud_platform

--- services/fetch/fetchImages.service.js
+++ services/fetch/fetchImages.service.js
@@ -20,8 +20,22 @@ module.exports = class fetchImagesService {
         if (!user) {
             return res.status(401).json({ error: true, message: "Unauthorized" });
         }
-        const { deviceId, fileLocation } = req.body;
-        const reqId = uuidv7();
+        const { id, deviceId, fileLocation } = req.body;
+        const reqId = id;
+        try {
+            const imageALreadyExists = await RedisConfiguration.getClient().exists(reqId);
+            if (imageALreadyExists) {
+                return res.status(200).json({
+                    success: true,
+                    message: "Sync request sent to device",
+                    requestId: reqId
+                });
+            }
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(500).json({ error: true, message: "Internal server error" });
+        }
         try {
             const getFcmTokenQuery = `SELECT devices.fcm_token 
 FROM users 
@@ -29,7 +43,7 @@ INNER JOIN devices ON users._id = devices.user_id
 WHERE users._id = ? AND devices.android_id = ?;`;
 
             const [rows] = await pool.execute(getFcmTokenQuery, [user.id, deviceId]);
-          
+
             if (rows.length === 0 || !rows[0].fcm_token) {
                 return res.status(404).json({ error: true, message: "Device not found or FCM token missing" });
             }
@@ -38,7 +52,7 @@ WHERE users._id = ? AND devices.android_id = ?;`;
             console.log("🚀 Sending notification to token:", targetFcmToken);
 
 
-            const { success, messageId } = await sendNotification(reqId, fileLocation, targetFcmToken);
+            const { success, messageId } = await sendNotification(reqId.toString(), fileLocation, targetFcmToken);
 
             if (success) {
                 return res.status(200).json({
@@ -64,14 +78,14 @@ WHERE users._id = ? AND devices.android_id = ?;`;
         try {
             const imageStream = await client.get(reqId);
 
-            if(imageStream){
-                return res.status(200).json({error : false,status : "completed", data : imageStream});
+            if (imageStream) {
+                return res.status(200).json({ error: false, status: "completed", data: imageStream });
             }
-            return res.status(200).json({error : false, status : "pending", message : "Image not found"});
+            return res.status(200).json({ error: false, status: "pending", message: "Image not found" });
         }
-        catch(err){
+        catch (err) {
             console.log(err);
-            return res.status(500).json({error : true, message : "internal server error"});
+            return res.status(500).json({ error: true, message: "internal server error" });
         }
     }
 };
